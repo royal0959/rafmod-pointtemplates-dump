4bdcefe4fa516020903b2a1e140bbe8b81b0ab16
royal0959 <70254342+royal0959@users.noreply.github.com>
2022-09-03 22:49:10 +0700

    modified to fit intended request

--- misc templates/the fat request spell stuff/the_fat_request.lua
+++ misc templates/the fat request spell stuff/the_fat_request.lua
@@ -12,46 +12,60 @@ local POSSIBLE_RARES = {
 	"Summon Skeletons",
 }
 
-function SpellGenerator(rareWeight, activator)
-	local timerId = false
-	local callbacks = {}
+function AddRandomSpell(rareWeight, activator)
+	local gen = math.random(1, 100)
 
-	local function stopGenerator()
-		timer.Stop(timerId)
-		for _, id in pairs(callbacks) do
-			activator:RemoveCallback(id)
-		end
+	if gen <= tonumber(rareWeight) then
+		local chosenSpell = POSSIBLE_RARES[math.random(#POSSIBLE_RARES)]
+		print(chosenSpell)
+		activator:AddSpell(chosenSpell)
+	else
+		local chosenSpell = POSSIBLE_COMMONS[math.random(#POSSIBLE_COMMONS)]
+		print(chosenSpell)
+		activator:AddSpell(chosenSpell)
 	end
-
-	local function addSpell(chosenSpell)
-		for _ = 1, CHARGES do
-			activator:AddSpell(chosenSpell)
-		end
-	end
-
-	timerId = timer.Create(SPELL_GEN_INTERVAL, function ()
-		if not activator:IsAlive() then
-		-- 	stopGenerator()
-			return
-		end
-
-		local gen = math.random(1, 100)
-
-		if gen <= tonumber(rareWeight) then
-			local chosenSpell = POSSIBLE_RARES[math.random(#POSSIBLE_RARES)]
-			print(chosenSpell)
-			addSpell(chosenSpell)
-		else
-			local chosenSpell = POSSIBLE_COMMONS[math.random(#POSSIBLE_COMMONS)]
-			print(chosenSpell)
-			addSpell(chosenSpell)
-		end
-	end, 0)
-
-	callbacks.onDeath = activator:AddCallback(ON_DEATH, function()
-		stopGenerator()
-	end)
-	callbacks.onSpawn = activator:AddCallback(ON_SPAWN, function()
-		stopGenerator()
-	end)
 end
+
+-- function SpellGenerator(rareWeight, activator)
+-- 	local timerId = false
+-- 	local callbacks = {}
+
+-- 	local function stopGenerator()
+-- 		timer.Stop(timerId)
+-- 		for _, id in pairs(callbacks) do
+-- 			activator:RemoveCallback(id)
+-- 		end
+-- 	end
+
+-- 	local function addSpell(chosenSpell)
+-- 		for _ = 1, CHARGES do
+-- 			activator:AddSpell(chosenSpell)
+-- 		end
+-- 	end
+
+-- 	timerId = timer.Create(SPELL_GEN_INTERVAL, function ()
+-- 		if not activator:IsAlive() then
+-- 		-- 	stopGenerator()
+-- 			return
+-- 		end
+
+-- 		local gen = math.random(1, 100)
+
+-- 		if gen <= tonumber(rareWeight) then
+-- 			local chosenSpell = POSSIBLE_RARES[math.random(#POSSIBLE_RARES)]
+-- 			print(chosenSpell)
+-- 			addSpell(chosenSpell)
+-- 		else
+-- 			local chosenSpell = POSSIBLE_COMMONS[math.random(#POSSIBLE_COMMONS)]
+-- 			print(chosenSpell)
+-- 			addSpell(chosenSpell)
+-- 		end
+-- 	end, 0)
+
+-- 	callbacks.onDeath = activator:AddCallback(ON_DEATH, function()
+-- 		stopGenerator()
+-- 	end)
+-- 	callbacks.onSpawn = activator:AddCallback(ON_SPAWN, function()
+-- 		stopGenerator()
+-- 	end)
+-- end
