4ca57db34eb923c2354730a267e7e75e15511b48
royal0959 <70254342+royal0959@users.noreply.github.com>
2023-06-25 00:37:30 +0700

    various fixes for rocket penetration + format

--- requests/damno request/rocket penetration/rocket_penetration.lua
+++ requests/damno request/rocket penetration/rocket_penetration.lua
@@ -1,10 +1,25 @@
 local BASE_DAMAGE = 70
+local WEAPON_STICKY_GRENADE = 3
 
-local function weaponMimic(properties, positional)
+local function weaponMimic(properties)
 	local mimic = ents.CreateWithKeys("tf_point_weapon_mimic", properties)
-	mimic:SetAbsOrigin(positional.Origin)
-	mimic:SetAbsAngles(positional.Angles)
 	return mimic
+end
+
+local function addGlobalDamageCallback(player)
+	-- Weird hack to fix conch not healing with penetration (thanks Orin xoxo)
+	player:AddCallback(ON_DAMAGE_RECEIVED_PRE, function(_, damageinfo)
+		-- Ugly way to check for rocket penetrations
+		if damageinfo.Inflictor:GetClassname() ~= "tf_projectile_pipe" then return end
+		if damageinfo.Weapon ~= nil then return end
+
+		damageinfo.Weapon = damageinfo.Attacker:GetPlayerItemBySlot(LOADOUT_POSITION_PRIMARY)
+		return true
+	end)
+end
+
+function OnPlayerConnected(player)
+	addGlobalDamageCallback(player)
 end
 
 ents.AddCreateCallback("tf_projectile_rocket", function(projectile)
@@ -15,34 +30,36 @@ ents.AddCreateCallback("tf_projectile_rocket", function(projectile)
 
 		local owner = projectile.m_hOwnerEntity
 		local primary = owner:GetPlayerItemBySlot(LOADOUT_POSITION_PRIMARY)
-
 		if primary:GetClassname() ~= "tf_weapon_rocketlauncher" then
 			return
 		end
 
 		local maxPenetrationCount = primary:GetAttributeValue("projectile penetration")
-
 		if not maxPenetrationCount then
 			return
 		end
 
+		local callback;
 		local isCrit = projectile.m_bCritical
-
-		local penetrations = 0
 		local penetrated = {}
-
-		local callback
-		callback = projectile:AddCallback(ON_SHOULD_COLLIDE, function(_, other)
-			if not other:IsPlayer() and not other:IsNPC() and not other:IsObject() then
+		local penetrations = 0
+		callback = projectile:AddCallback(ON_SHOULD_COLLIDE, function(_, collided)
+			-- CTFPlayer, then NextBotCombatCharacter, then CTFBaseObject
+			if not collided:IsPlayer() and not collided:IsNPC() and not collided:IsObject() then
 				return
 			end
 
-			local index = other:GetHandleIndex()
+			if collided.m_iTeamNum == owner.m_iTeamNum then
+				return
+			end
 
+			-- Make sure it hasn't been penetrated already
+			local index = collided:GetHandleIndex()
 			if penetrated[index] then
 				return false
 			end
 
+			-- Boring shit
 			if penetrations >= maxPenetrationCount then
 				projectile:RemoveCallback(callback)
 				return
@@ -51,34 +68,27 @@ ents.AddCreateCallback("tf_projectile_rocket", function(projectile)
 			penetrations = penetrations + 1
 			penetrated[index] = true
 
+			-- Good shit
 			local damageBonusMult = primary:GetAttributeValueByClass("mult_dmg", 1)
 			local damage = BASE_DAMAGE * damageBonusMult
 
-			local damageType = DMG_BLAST
-			if isCrit == 1 then
-				damageType = damageType + DMG_CRITICAL
-			end
-
 			local mimic = weaponMimic({
 				TeamNum = owner.m_iTeamNum,
-				WeaponType = 3, -- sticky
+				WeaponType = WEAPON_STICKY_GRENADE, -- sticky
+				Damage = damage,
 				Crits = isCrit,
-
+				SplashRadius = 146,
 				SpeedMax = 0,
 				SpeedMin = 0,
-
-				SplashRadius = 146,
-				Damage = damage,
-			}, {
-				Origin = projectile:GetAbsOrigin(),
-				Angles = Vector(0, 0, 0),
+				["$dmgtype"] = 69420,
+				["$killicon"] = "player_penetration",
+				origin = tostring(projectile:GetAbsOrigin()),
+				angles = tostring(Vector(0, 0, 0)),
 			})
 
 			mimic["$SetOwner"](mimic, owner)
-
 			mimic:FireOnce()
 			mimic:DetonateStickies()
-
 			timer.Simple(0.1, function()
 				mimic:Remove()
 			end)
